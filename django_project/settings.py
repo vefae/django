b10e567b6105fb5b3bcc3f457af2dccd2c523f33
vefa <vgulecyuz@gmail.com>
2018-11-14 16:30:19 +0100

    heroku

--- django_project/settings.py
+++ django_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -140,3 +141,6 @@ STATICFILES_DIRS = [
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
 LOGIN_URL = 'first_app/login'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
